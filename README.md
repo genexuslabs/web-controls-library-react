a5d3f91833e217afb3b44fe744301ae6c026e8c6
Nicolas Cardelino <ncardeli@gmail.com>
2020-12-16 16:41:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# @genexus/web-controls-library-react
+
+This is a React specific wrapper for [@genexus/web-controls-library](https://github.com/genexuslabs/web-controls-library).
+
+## Getting Started
+
+```bash
+npm install
+npm start
+```
+
+## Building for production
+
+To build the library for production, run:
+
+```bash
+npm run build
+```
+
+## Using this components library
+
+### Node Modules
+
+- Run `npm install @genexus/web-controls-library-react --save`
+- `import { GxButton } from '@genexus/web-controls-library-react';`
---
+++ src/components.ts
@@ -0,0 +1,57 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@genexus/web-controls-library';
+
+
+
+export const GxActionSheet = /*@__PURE__*/createReactComponent<JSX.GxActionSheet, HTMLGxActionSheetElement>('gx-action-sheet');
+export const GxActionSheetItem = /*@__PURE__*/createReactComponent<JSX.GxActionSheetItem, HTMLGxActionSheetItemElement>('gx-action-sheet-item');
+export const GxAudio = /*@__PURE__*/createReactComponent<JSX.GxAudio, HTMLGxAudioElement>('gx-audio');
+export const GxBootstrap = /*@__PURE__*/createReactComponent<JSX.GxBootstrap, HTMLGxBootstrapElement>('gx-bootstrap');
+export const GxButton = /*@__PURE__*/createReactComponent<JSX.GxButton, HTMLGxButtonElement>('gx-button');
+export const GxCanvas = /*@__PURE__*/createReactComponent<JSX.GxCanvas, HTMLGxCanvasElement>('gx-canvas');
+export const GxCanvasCell = /*@__PURE__*/createReactComponent<JSX.GxCanvasCell, HTMLGxCanvasCellElement>('gx-canvas-cell');
+export const GxCard = /*@__PURE__*/createReactComponent<JSX.GxCard, HTMLGxCardElement>('gx-card');
+export const GxCheckbox = /*@__PURE__*/createReactComponent<JSX.GxCheckbox, HTMLGxCheckboxElement>('gx-checkbox');
+export const GxChronometer = /*@__PURE__*/createReactComponent<JSX.GxChronometer, HTMLGxChronometerElement>('gx-chronometer');
+export const GxEdit = /*@__PURE__*/createReactComponent<JSX.GxEdit, HTMLGxEditElement>('gx-edit');
+export const GxFormField = /*@__PURE__*/createReactComponent<JSX.GxFormField, HTMLGxFormFieldElement>('gx-form-field');
+export const GxGridEmptyIndicator = /*@__PURE__*/createReactComponent<JSX.GxGridEmptyIndicator, HTMLGxGridEmptyIndicatorElement>('gx-grid-empty-indicator');
+export const GxGridFs = /*@__PURE__*/createReactComponent<JSX.GxGridFs, HTMLGxGridFsElement>('gx-grid-fs');
+export const GxGridHorizontal = /*@__PURE__*/createReactComponent<JSX.GxGridHorizontal, HTMLGxGridHorizontalElement>('gx-grid-horizontal');
+export const GxGridInfiniteScroll = /*@__PURE__*/createReactComponent<JSX.GxGridInfiniteScroll, HTMLGxGridInfiniteScrollElement>('gx-grid-infinite-scroll');
+export const GxGridInfiniteScrollContent = /*@__PURE__*/createReactComponent<JSX.GxGridInfiniteScrollContent, HTMLGxGridInfiniteScrollContentElement>('gx-grid-infinite-scroll-content');
+export const GxGridSmartCell = /*@__PURE__*/createReactComponent<JSX.GxGridSmartCell, HTMLGxGridSmartCellElement>('gx-grid-smart-cell');
+export const GxGridSmartCss = /*@__PURE__*/createReactComponent<JSX.GxGridSmartCss, HTMLGxGridSmartCssElement>('gx-grid-smart-css');
+export const GxGroup = /*@__PURE__*/createReactComponent<JSX.GxGroup, HTMLGxGroupElement>('gx-group');
+export const GxIcon = /*@__PURE__*/createReactComponent<JSX.GxIcon, HTMLGxIconElement>('gx-icon');
+export const GxImage = /*@__PURE__*/createReactComponent<JSX.GxImage, HTMLGxImageElement>('gx-image');
+export const GxInteractiveImage = /*@__PURE__*/createReactComponent<JSX.GxInteractiveImage, HTMLGxInteractiveImageElement>('gx-interactive-image');
+export const GxLayout = /*@__PURE__*/createReactComponent<JSX.GxLayout, HTMLGxLayoutElement>('gx-layout');
+export const GxLoading = /*@__PURE__*/createReactComponent<JSX.GxLoading, HTMLGxLoadingElement>('gx-loading');
+export const GxLottie = /*@__PURE__*/createReactComponent<JSX.GxLottie, HTMLGxLottieElement>('gx-lottie');
+export const GxMap = /*@__PURE__*/createReactComponent<JSX.GxMap, HTMLGxMapElement>('gx-map');
+export const GxMapLine = /*@__PURE__*/createReactComponent<JSX.GxMapLine, HTMLGxMapLineElement>('gx-map-line');
+export const GxMapMarker = /*@__PURE__*/createReactComponent<JSX.GxMapMarker, HTMLGxMapMarkerElement>('gx-map-marker');
+export const GxMessage = /*@__PURE__*/createReactComponent<JSX.GxMessage, HTMLGxMessageElement>('gx-message');
+export const GxModal = /*@__PURE__*/createReactComponent<JSX.GxModal, HTMLGxModalElement>('gx-modal');
+export const GxNavbar = /*@__PURE__*/createReactComponent<JSX.GxNavbar, HTMLGxNavbarElement>('gx-navbar');
+export const GxNavbarItem = /*@__PURE__*/createReactComponent<JSX.GxNavbarItem, HTMLGxNavbarItemElement>('gx-navbar-item');
+export const GxPasswordEdit = /*@__PURE__*/createReactComponent<JSX.GxPasswordEdit, HTMLGxPasswordEditElement>('gx-password-edit');
+export const GxProgressBar = /*@__PURE__*/createReactComponent<JSX.GxProgressBar, HTMLGxProgressBarElement>('gx-progress-bar');
+export const GxRadioGroup = /*@__PURE__*/createReactComponent<JSX.GxRadioGroup, HTMLGxRadioGroupElement>('gx-radio-group');
+export const GxRadioOption = /*@__PURE__*/createReactComponent<JSX.GxRadioOption, HTMLGxRadioOptionElement>('gx-radio-option');
+export const GxRating = /*@__PURE__*/createReactComponent<JSX.GxRating, HTMLGxRatingElement>('gx-rating');
+export const GxSelect = /*@__PURE__*/createReactComponent<JSX.GxSelect, HTMLGxSelectElement>('gx-select');
+export const GxSelectOption = /*@__PURE__*/createReactComponent<JSX.GxSelectOption, HTMLGxSelectOptionElement>('gx-select-option');
+export const GxSwitch = /*@__PURE__*/createReactComponent<JSX.GxSwitch, HTMLGxSwitchElement>('gx-switch');
+export const GxTab = /*@__PURE__*/createReactComponent<JSX.GxTab, HTMLGxTabElement>('gx-tab');
+export const GxTabCaption = /*@__PURE__*/createReactComponent<JSX.GxTabCaption, HTMLGxTabCaptionElement>('gx-tab-caption');
+export const GxTabPage = /*@__PURE__*/createReactComponent<JSX.GxTabPage, HTMLGxTabPageElement>('gx-tab-page');
+export const GxTable = /*@__PURE__*/createReactComponent<JSX.GxTable, HTMLGxTableElement>('gx-table');
+export const GxTableCell = /*@__PURE__*/createReactComponent<JSX.GxTableCell, HTMLGxTableCellElement>('gx-table-cell');
+export const GxTextblock = /*@__PURE__*/createReactComponent<JSX.GxTextblock, HTMLGxTextblockElement>('gx-textblock');
+export const GxVideo = /*@__PURE__*/createReactComponent<JSX.GxVideo, HTMLGxVideoElement>('gx-video');
